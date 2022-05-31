79a911a9c9a2eb3deb89f7243c8ab96850b45da6
LB Lane <lb@bolt.com>
2022-05-31 15:16:03 -0400

    handle ref links in parameters

--- index.js
+++ index.js
@@ -126,20 +126,25 @@ const generatePathMethods = (path) => {
     // Generate the html for each method and store it in the variables above.
     if (get_raw){
         const parameters = generateMethodParameters(get_raw)
-
+        const request_bodies = generateResponses(get_raw)
         get_html = `<div class="get">
         <h3>GET</h3>
         <p>${get_raw.description}</p>
         <p>${get_raw.tags}</p>
         <p>${get_raw.operationId}</p>
-            <div class="parameters">
+        <div class="parameters">
             <h4>Parameters</h4>
             ${parameters}
-            </div>
+        </div>
+        <div class="request_body">
+            <h4>Request Body</h4>
+            ${request_bodies}
+        </div>
         </div>`
     }
     if (post_raw){
         const parameters = generateMethodParameters(post_raw)
+        const request_bodies = generateResponses(post_raw)
         post_html = `<div class="post">
         <h3>POST</h3>
         <p>${post_raw.description}</p>
@@ -148,11 +153,16 @@ const generatePathMethods = (path) => {
         <div class="parameters">
             <h4>Parameters</h4>
             ${parameters}
-            </div>
+        </div>
+        <div class="request_body">
+            <h4>Request Body</h4>
+            ${request_bodies}
+        </div>
         </div>`
     }
     if (put_raw){
         const parameters = generateMethodParameters(put_raw)
+        const request_bodies = generateResponses(put_raw)
         put_html = `<div class="put">
         <h3>PUT</h3>
         <p>${put_raw.description}</p>
@@ -162,10 +172,15 @@ const generatePathMethods = (path) => {
             <h4>Parameters</h4>
             ${parameters}
             </div>
+        <div class="request_body">
+            <h4>Request Body</h4>
+            ${request_bodies}
+        </div>
         </div>`
     }
     if (delete_raw){
         const parameters = generateMethodParameters(delete_raw)
+        const request_bodies = generateResponses(delete_raw)
         delete_html = `<div class="delete">
         <h3>DELETE</h3>
         <p>${delete_raw.description}</p>
@@ -175,6 +190,10 @@ const generatePathMethods = (path) => {
             <h4>Parameters</h4>
             ${parameters}
             </div>
+        <div class="request_body">
+            <h4>Request Body</h4>
+            ${request_bodies}
+         </div>
         </div>`
     }
     let all_methods = `${get_html} ${post_html} ${put_html} ${delete_html}`
@@ -185,16 +204,51 @@ const generateMethodParameters = (method) => {
     const method_parameters = []
     if (method.parameters){
         method.parameters.forEach(parameter => {
+            // if parameter is $ref, get the parameter name from the ref
+            if (parameter.$ref){
+                const ref_name = parameter.$ref.split('/')[3]
+                const ref_link = specJson.components.parameters[ref_name]
+                console.log(ref_link)
+                method_parameters.push(
+                    `<div class="parameter">
+                    <h5>${ref_link.name}</h5>
+                    <p><strong>Type:</strong> ${ref_link.in}</p>
+                    <p><strong>Description:</strong> ${ref_link.description}</p>
+                    <p><strong>Required:</strong> ${ref_link.required}</p>
+                    </div>`
+                )
+            } else {
             method_parameters.push(`<div class="parameter">
             <h5>${parameter.name}</h5>
             <p><strong>Type:</strong> ${parameter.in}</p>
             <p><strong>Description:</strong> ${parameter.description}</p>
             <p><strong>Required:</strong> ${parameter.required}</p>
             </div>`)
+            }
         })
     }
     return method_parameters.join('')
 }
+
+
+const generateResponses = (method) => {
+    let responses_content = ''
+    if (method.responses){
+        const response = method.responses 
+        const response_keys = Object.keys(response)
+        response_keys.forEach(key => {
+            console.log(key)
+            responses_content += `<div class="response">
+            <h5>${key}</h5>
+            <p><strong>Description:</strong> ${response[key].description}</p>
+            <p><strong>Schema:</strong> ${key}</p>
+            </div>`
+        })
+    }
+
+    return responses_content
+}
+
 
 // Create the index.html file for the documentation.
 const updateHtmlFile = () => {
